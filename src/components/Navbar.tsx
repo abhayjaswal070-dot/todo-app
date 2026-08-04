365158166897df8d1745582fb3b7cbaa6b88b50f
Abhay Jaswal <abhayjaswal070@gmail.com>
2026-08-04 17:18:32 +0530

    new updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
 
   return (
     <>
-      <div className="flex justify-between px-10 py-2 bg-gray-700 text-white">
+      <div className="flex justify-between px-12 py-2 bg-gray-700 text-white">
         <h1 onClick={() => navigate('/')} className="text-2xl cursor-pointer">Todo</h1>
         <button onClick={() => navigate('/add')} className="px-3 py-3 cursor-pointer active:scale-95 bg-gray-800 text-white rounded">Add Task</button>
       </div>
